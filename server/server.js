074a55f7533db3df6b832fd941f7c716b0b94d15
viniciuspontes <vipontes70@gmail.com>
2020-10-24 15:51:10 -0300

    Implementing socket connection/disconnection

--- server/server.js
+++ server/server.js
@@ -1,11 +1,28 @@
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
 const path = require('path');
 const publicPath = path.join(__dirname, '/../public');
 const port = process.env.PORT || 3000
-var app = express();
+
+let app = express();
+let server = http.createServer(app);
+
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log("New user just connected!");
+
+    socket.on('disconnect', () => {
+        console.log("User was disconnected!");
+    });
+});
+
+
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
